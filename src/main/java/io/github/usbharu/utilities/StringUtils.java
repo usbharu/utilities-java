8563721bd16ee9d954f4a179e3e7e6e5009c2968
usbharu <64310155+usbharu@users.noreply.github.com>
2022-05-13 06:48:27 +0900

    removeEmptyStringsを追加

--- src/main/java/io/github/usbharu/utilities/StringUtils.java
+++ src/main/java/io/github/usbharu/utilities/StringUtils.java
@@ -18,4 +18,33 @@ public class StringUtils {
     return str.substring(0, index);
   }
 
+  /**
+   * Remove all empty strings from the given array.
+   *
+   * @param strings the array to be cleaned
+   * @return the cleaned array
+   */
+  public static String[] removeEmptyStrings(String[] strings) {
+    if (strings == null) {
+      return new String[0];
+    }
+    int size = strings.length;
+    int newSize = 0;
+    for (String s : strings) {
+      if (s != null && s.length() > 0) {
+        newSize++;
+      }
+    }
+    if (newSize == size) {
+      return strings;
+    }
+    String[] newString = new String[newSize];
+    int j = 0;
+    for (String s : strings) {
+      if (s != null && s.length() > 0) {
+        newString[j++] = s;
+      }
+    }
+    return newString;
+  }
 }
